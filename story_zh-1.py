ad4f211e494d517c9cc5d2d6d7672d106949ae6d
Reed Riggs <78134431+reedriggs@users.noreply.github.com>
2021-02-07 08:22:00 -1000

    Update story_zh-1.py

--- story_zh-1.py
+++ story_zh-1.py
@@ -6,5 +6,8 @@
 用法乙 = '省很多時間'
 
 故事一 = f"{甲}和{乙}很期待搬到新的家。" + "\n" + "\n" + f"{甲}特別想用剛買的{品甲}，因為{用法甲}。" + "\n" "\n" + f"{乙}很期待新的{品乙}，因為可以{用法乙}！"
+故事二 = f"{甲}和{乙}很期待搬到新的辦公室。" + "\n" + "\n" + f"{甲}特別想用辦公室的的{品甲}，因為{用法甲}。" + "\n" "\n" + f"{乙}很期待用幫共識的{品乙}，因為可以{用法乙}！"
 
 print(故事一)
+#this second version only changes the surrounding text and story content, but not the content of the blanks that are filled in.
+print(故事二)
