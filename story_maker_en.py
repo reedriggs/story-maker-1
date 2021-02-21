ed2f476e132e4edcc50b984a3ad24080f040ce02
Reed Riggs <78134431+reedriggs@users.noreply.github.com>
2021-02-20 22:56:24 -1000

    Update story_maker_en.py

--- story_maker_en.py
+++ story_maker_en.py
@@ -509,8 +509,8 @@ room = Room_en(rooms_distr[placeR])
 
 #Modify plot template(s) here:
 #Act 1, where home is introduced and then disrupted
-scene1a = f"\"Wait!,\" {protag1.name} said, \"We were just here.\" \"I was standing over there last time,\" {protag1.pron1} said. {protag1.name} tried to remember what was happening just a few moments before, but memories slipped away much like right after waking up from a dream. And then everything ended again."
-scene2a = f"In a house in Houston, Texas, in the {room.name}, {protag1.name} looked at {ally1.name} and {fake_ally_opp_1.name}. {protag1.name} thought something seemed strange. \"What were we doing just now, or, just before now?\" {ally1.name} and {fake_ally_opp_1.name} had no idea. \"Are you hungry? Should we eat?\" {protag1.name} asked. \"Sure,\" {ally1.name} answered, but just then things came to an end."
+Act1_scene1a = f"\"Wait!,\" {protag1.name} said, \"We were just here.\" \"I was standing over there last time,\" {protag1.pron1} said. {protag1.name} tried to remember what was happening just a few moments before, but memories slipped away much like right after waking up from a dream. And then everything ended again."
+Act1_scene2a = f"In a house in Houston, Texas, in the {room.name}, {protag1.name} looked at {ally1.name} and {fake_ally_opp_1.name}. {protag1.name} thought something seemed strange. \"What were we doing just now, or, just before now?\" {ally1.name} and {fake_ally_opp_1.name} had no idea. \"Are you hungry? Should we eat?\" {protag1.name} asked. \"Sure,\" {ally1.name} answered, but just then things came to an end."
 
 #Act 2, where the characters try to understand and fix the disruption, finding small success
 
@@ -519,6 +519,7 @@ scene2a = f"In a house in Houston, Texas, in the {room.name}, {protag1.name} loo
   #create as foils
 
 #Act 4, where the characters hatch a new plan to escape
+Act4_scene1a = f"\"I'm fine here,\" said {protag1.name}. \"I know you all want to escape and whatever, but I have so much I can focus on and get done if I'm not spending all of my time trying to escape.\" Just then, everything ended again."
 
 #Act 5, where each character deals with their new knowledge of the code-world in which they live
 
@@ -527,8 +528,8 @@ plot_z = f'{protag1.name} looked at {protag1.pron3} hands. {protag1.pron1.capita
 
 #Create dictionaries containing plot templates
 plots = {
-    1:scene1a,
-    2:scene2a,
+    1:Act1_scene1a,
+    2:Act1_scene2a,
     3:plot_y,
     4:plot_z
 }
